fe9c0536093228871c3e5ce57ed518221fe54a41
Felipe R Pinto <resendepinto@gmail.com>
2019-12-27 17:21:54 -0300

    Update challenge-10.js

--- challenge-10/challenge-10.js
+++ challenge-10/challenge-10.js
@@ -9,11 +9,11 @@ deles seja "true", usando os Wrapper Objects como "conversores" nos valores
 das variáveis. Analise o que está sendo impresso no console para saber como
 resolver o problema corretamente.
 */
-var five = '5';
-console.log( five + ' é número?', typeof five === 'number' );
+var five = Number('5');
+console.log(five + ' é número?', typeof five === 'number');
 
-var concat = 10 + 10;
-console.log( '"' + concat + '" é uma string? E é igual a "1010"?', typeof concat === 'string' );
+var concat = String(10) + String(10);
+console.log('"' + concat + '" é uma string? E é igual a "1010"?', typeof concat === 'string');
 
 /*
 Voltando ao exemplo da calculadora, vamos utilizar mais uma abordagem
@@ -24,7 +24,23 @@ funcional, mas dessa vez, separando algumas responsabilidades.
 função receberá dois parâmetros e retornará a operação referente à sua
 propriedade, usando os valores passados por parâmetro.
 */
-// ?
+var operation = {
+	'+': function sum(num1, num2) {
+		return num1 + num2;
+	},
+	'-': function subtract(num1, num2) {
+		return num1 - num2;
+	},
+	'*': function multi(num1, num2) {
+		return num1 * num2;
+	},
+	'/': function division(num1, num2) {
+		return mum1 / num2;
+	},
+	'%': function mod(num1, num2) {
+		return x % y;
+	},
+};
 
 /*
 Crie uma função chamada `isOperatorValid`, que receberá um operador por
@@ -36,8 +52,10 @@ parâmetro a ela é válido, ou seja, se ele é igual a '+', '-', '*', '/' ou
 Caso contrário, "false".
 - O desafio é fazer o retorno sem usar "if" ou "switch".
 */
-// ?
-
+function isOperatorValid(operator) {
+	return !!operation[operator];
+}
+console.log(isOperatorValid('+'));
 /*
 Agora vamos criar a calculadora.
 - Crie uma função chamada `calculator`, que receberá como parâmetro um
@@ -50,7 +68,17 @@ parâmetros;
 operador passado para a função "calculator", e passando para esse método
 os dois parâmetros da função de retorno de "calculator".
 */
-// ?
+function calculator(operator) {
+	if (!isOperatorValid(operator)) {
+		return false;
+	}
+	return function(x, y) {
+		if (typeof x !== 'number' || typeof y !== 'number') {
+			return false;
+		}
+		return operation[operator](x, y);
+	};
+}
 
 /*
 Crie uma função chamada "showOperationMessage" que recebe três parâmetros:
@@ -59,7 +87,9 @@ deve ser a frase:
 'A operação [NUMBER1] [OPERATOR] [NUMBER2] =';
 Essa função mostrará a mensagem da operação que criaremos mais abaixo.
 */
-// ?
+function showOperationMessage(operator, mumber1, number2) {
+	return 'A operação ' + number1 + ' ' + operator + ' ' + number2 + ' =';
+}
 
 /*
 Crie uma função chamada "showErrorMessage" que recebe um parâmetro: o
@@ -67,7 +97,9 @@ operador da operação cálculo, quando a operação não for válida.
 Essa função deverá retornar a frase:
 'Operação "[OPERATOR]" não permitida!'
 */
-// ?
+function showErrorMessage(operator) {
+	return 'Operação' + operator + 'não permitida!';
+}
 
 /*
 Nossa calculadora está pronta! Agora vamos testá-la:
@@ -75,7 +107,9 @@ PASSO 1:
 - Declare 3 variáveis: "number1" e "number2", iniciando com valor zero, e
 "operationSignal", sem valor por enquanto.
 */
-// ?
+var number1 = 0;
+var number2 = 0;
+var operationSignal;
 
 /*
 PASSO 2:
@@ -83,7 +117,8 @@ Atribua à variável operationSignal o operador de soma, e declare uma
 variável chamada "sum", que receba a função "calculator", passando por
 parâmetro a variável que recebeu o sinal da operação.
 */
-// ?
+operationSignal = '+';
+var sum = calculator(operationSignal);
 
 /*
 PASSO 3:
@@ -97,7 +132,13 @@ parâmetros para o método "log" de "console":
 - O segundo, a função de soma, passando os dois operandos.
 - Se "sum" for "false", mostrar no console a mensagem de erro.
 */
-// ?
+if (sum) {
+	number1 = 10;
+	number2 = 12;
+	console.log(showOperationMessage(operationSignal, number1, number2), sum(number1, number2));
+} else {
+	console.log(showErrorMessage(operationSignal));
+}
 
 /*
 Repita desde o "PASSO 2" com as operações de subtração, multiplicação,
